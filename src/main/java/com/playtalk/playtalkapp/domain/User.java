09e8eb5dbc05a4af4281ded4977375695d03d1a1
HINZOO <hinge1128@gmail.com>
2023-10-19 22:06:35 +0900

    JPA데이터 기준파일

---
+++ src/main/java/com/playtalk/playtalkapp/domain/User.java
@@ -0,0 +1,30 @@
+package com.playtalk.playtalkapp.domain;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    private String user_id;
+    @Column(name="pw",nullable=false)//null을 허용하지 않는다.
+    private String pw;
+
+    private String nkname;
+    private String email;
+    private String phone_number;
+    @CreationTimestamp
+    private LocalDateTime signup_date;
+    //Status
+
+    private String email_check_code;
+
+    //만약에 autoIncrement가 있는 컬럼인경우
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int index;
+
+}
