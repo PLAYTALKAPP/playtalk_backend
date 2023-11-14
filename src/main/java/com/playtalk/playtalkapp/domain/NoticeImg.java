87d7fe988f69d5c65c4503ac6fc1b10eeb247d93
mlulucky <dmswjdans@gmail.com>
2023-11-14 21:20:56 +0900

    refactor: noticeImg 엔티티 img_id 컬럼명 변경 및 img_main 컬럼 추가

--- src/main/java/com/playtalk/playtalkapp/domain/NoticeImg.java
+++ src/main/java/com/playtalk/playtalkapp/domain/NoticeImg.java
@@ -12,9 +12,10 @@ import lombok.NoArgsConstructor;
 public class NoticeImg {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long notice_img_id;
+    private Long img_id;
     @Column(nullable = false)
     private String img_path;
+    private boolean img_main;
     @ManyToOne // notice_img 입장에서 notice_img : notice (다대일)
     @JoinColumn(name = "notice_id") // 어떤 notice 의 img 인지 알아야 하기 때문에
     private Notice notice;
