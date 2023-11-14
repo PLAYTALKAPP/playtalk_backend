4153ab78124a208b40a717d61b25164f0178c910
Eunjeong Moon <117883588+mlulucky@users.noreply.github.com>
2023-11-14 21:27:44 +0900

    Feat/notice entity (#6)
    * JPA데이터 기준파일
    
    * feat: 엔티티 어노테이션 추가
    
    * feat: notice, notice_img 엔티티 생성
    
    * feat: notice 엔티티 noticeImgs 연관관계 추가
    
    * style: 공백삭제
    
    * feat: user 엔티티 noticeList 연관관계 추가
    
    * refactor: noticeImg 엔티티 img_id 컬럼명 변경 및 img_main 컬럼 추가
    
    ---------
    
    Co-authored-by: HINZOO <hinge1128@gmail.com>
    Co-authored-by: HIMJU <124220712+HINZOO@users.noreply.github.com>

--- src/main/java/com/playtalk/playtalkapp/domain/User.java
+++ src/main/java/com/playtalk/playtalkapp/domain/User.java
@@ -1,30 +1,42 @@
 package com.playtalk.playtalkapp.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     private String user_id;
     @Column(name="pw",nullable=false)//null을 허용하지 않는다.
     private String pw;
-
     private String nkname;
     private String email;
     private String phone_number;
     @CreationTimestamp
     private LocalDateTime signup_date;
-    //Status
+    private String email_check_code;
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY) // 클래스간의 양방향관계. 관계 주인(외래키 관리 주인)이 아닌 엔티티 클래스명 -> mappedBy = 엔티티 클래스명 // getNoticeList 할때 조인 (조회할때 조인 _ 지연로딩)
+    private List<Notice> noticeList;
 
     private String email_check_code;
+
 
     //만약에 autoIncrement가 있는 컬럼인경우
 //    @GeneratedValue(strategy = GenerationType.IDENTITY)
 //    private int index;
 
 }
+
